b7b14e9dbac573dc29faabd282a2c291dd394793
thetthetaye <thethet.aye@innov8te.com.sg>
2017-08-28 18:02:10 +0800

    Add migration file

---
+++ database/migrations/2017_08_28_084341_create_tbl_nric_code.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class CreateTblNricCode extends Migration {
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up() {
+		Schema::create('nric_codes', function (Blueprint $table) {
+			$table->increments('id');
+			$table->integer('nric_code');
+			$table->string('description');
+			$table->enum('deleted', ['N', 'Y']);
+			$table->integer('created_by');
+			$table->integer('updated_by');
+			$table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down() {
+		Schema::drop('nric_codes');
+	}
+}
---
+++ database/migrations/2017_08_28_084646_create_tbl_nric_township.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class CreateTblNricTownship extends Migration {
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up() {
+		Schema::create('nric_townships', function (Blueprint $table) {
+			$table->increments('id');
+			$table->integer('nric_code_id');
+			$table->string('township');
+			$table->string('short_name');
+			$table->integer('order');
+			$table->enum('deleted', ['N', 'Y']);
+			$table->integer('created_by');
+			$table->integer('updated_by');
+			$table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down() {
+		Schema::drop('nric_townships');
+	}
+}
